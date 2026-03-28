eecc39c9d3e925a82ba9d7d4f3d278350806a148
Ranim Mimouna <ranim.mimouna26@gmail.com>
2026-03-29 00:53:06 +0100

    make it batter

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -9,20 +13,47 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
-    const error = new HttpError('Could not find this route.', 404);
-    throw error;
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
 });
 
 app.use((error, req, res, next) => {
-    if (res.headerSent) {
-        return next(error);
-    }
-    res.status(error.code || 500);
-    res.json({message: error.message || 'An unknown error occured!'});
+  if (req.file) {
+    fs.unlink(req.file.path, err => {
+      console.log(err);
+    });
+  }
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
-app.listen(5000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.efvejrk.mongodb.net/${process.env.DB_NAME}?appName=Cluster0`
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const { check } = require('express-validator');
 
-const usersController = require('../contorllers/users-controller');
+const usersController = require('../contorllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
 router.get('/', usersController.getUsers);
 
 router.post
-    ('/signup', [
+    ('/signup', 
+        fileUpload.single('image'),
+    [
         check('name')
             .not()
             .isEmpty(),
