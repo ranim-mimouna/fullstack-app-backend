65507bd745917f39d96f3922093a1c7fc3996f33
Ranim Mimouna <ranim.mimouna26@gmail.com>
2026-03-29 01:05:12 +0100

    fix heroku port

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.efvejrk.mongodb.net/${process.env.DB_NAME}?appName=Cluster0`
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch(err => {
     console.log(err);
